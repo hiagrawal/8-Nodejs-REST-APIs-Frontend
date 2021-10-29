d576bc89b036dce22288bd7b59b808d9a32cf991
hiagrawal <agrawal.hina13@gmail.com>
2021-10-29 20:33:41 +0530

    Adding auth middlware to all routes

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -138,7 +138,10 @@ class Feed extends Component {
       //   title: postData.title,
       //   content: postData.content
       // })
-      body: formData
+      body: formData,
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -191,7 +194,10 @@ class Feed extends Component {
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
     fetch('http://localhost:8080/feed/post/' + postId , {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
